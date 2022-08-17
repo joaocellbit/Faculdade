fa706395e4112cea8f877577c93006a6f4dc54d3
joaocellbit <85044187+joaocellbit@users.noreply.github.com>
2022-08-17 10:24:06 -0300

    Add files via upload

--- maior numero.py
+++ maior numero.py
@@ -1,5 +1,10 @@
+#leitura dos dados inseridos pelo usuario
+
 numero_1 = int(input("numero 1"))
 numero_2 = int(input("numero 2"))
+
+#determinacao do maior numero
+
 if numero_1 > numero_2:
     print(numero_1, "eh o maior numero")
 elif numero_2 > numero_1:
