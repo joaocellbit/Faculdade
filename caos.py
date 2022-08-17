bbb662c256e42fbb71bde50ba7c455323442b6ed
joaocellbit <85044187+joaocellbit@users.noreply.github.com>
2022-08-17 12:37:33 -0300

    Update caos.py

--- caos.py
+++ caos.py
@@ -3,10 +3,12 @@ i = 0
 idadeMaior = 0
 nomeMaior = ""
 while(i<quantidade):
-    nome_1 = input("nome: ")
-    idade_1 = int(input("idade: "))
+    print('Nome do animal', i+1, ':')
+    nome_1 = input()
+    print("idade do animal ", i+1, ":")
+    idade_1 = int(input())
     if idade_1 > idadeMaior:
         idadeMaior = idade_1
         nomeMaior = nome_1
     i = i+1
-print(nomeMaior)
+print(nomeMaior, "eh o mais velho")
