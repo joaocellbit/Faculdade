362fbda6b56534c41b248131df022c0f5e00c1a7
joaocellbit <85044187+joaocellbit@users.noreply.github.com>
2022-08-17 11:13:35 -0300

    Add files via upload

--- animal.py
+++ animal.py
@@ -1,24 +1,24 @@
 print("animal 1")
 nome_1 = input("nome: ")
 idade_1 = int(input("idade: "))
+
+MaiorIdade = idade_1
+MaiorNome = nome_1
+
 print("animal 2")
 nome_2 = input("nome: ")
 idade_2 = int(input("idade: "))
+
+if idade_2 > MaiorIdade:
+    MaiorIdade = idade_2
+    MaiorNome = nome_2
+
 print("animal 3")
 nome_3 = input("nome: ")
 idade_3 = int(input("idade: "))
 
-if idade_1>idade_2>idade_3:
-    print(nome_1)
-elif idade_1>idade_3>idade_2:
-    print(nome_1)
-elif idade_2>idade_1>idade_3:
-    print (nome_2)
-elif idade_2>idade_3>idade_1:
-    print(nome_2)
-elif idade_3>idade_1>idade_2:
-    print(nome_3)
-elif idade_3>idade_2>idade_1:
-    print(nome_3)
-else:
-    print("tem pelomenos 2 iguais")
+if idade_3 > MaiorIdade:
+    MaiorIdade = idade_3
+    MaiorNome = nome_3
+    
+print("o", MaiorNome, "eh o mais velho")
